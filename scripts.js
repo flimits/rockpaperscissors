c8fa42ac1877c4e7d8d1ef4b9d574b41df9edd6b
Jason Savoie <flimits@gmail.com>
2023-09-28 08:29:08 -0700

    Got function to get computer response to work as well as figuring out who wins

--- scripts.js
+++ scripts.js
@@ -1,7 +1,9 @@
 // Let's play a gave of Rock, Paper and Scissors
 
 var playerScores = [, ,]; // wins and losses and ties
-var responseResult = "";
+var responseResult = ""; // Store the users response
+var compResp = ""; // Store the computers response
+var validValues = ["r", "p", "s"];
 
 // function getUserResponse() {
 // var userResponse = prompt("Enter one of either R, S or P to play ...");
@@ -11,8 +13,6 @@ function getUserResponse(userResponse) {
 
    var userResp = prompt("Enter one of either R, S or P to play ...");
    responseResult = userResp.toLowerCase();
-   // alert("You entered " + responseResult);
-
    if (responseResult === "r" || responseResult === "s" || responseResult == "p") {
       console.log(responseResult + " is a good entry is ok");
    } else {
@@ -22,4 +22,35 @@ function getUserResponse(userResponse) {
 }
 
 getUserResponse();
-// console.log(responseResult);
+
+function computerResponse() {
+   var indexNumber = Math.floor(Math.random() * validValues.length);
+   compResp = validValues[indexNumber];
+   console.log(compResp + " is what the computer says");
+}
+
+computerResponse();
+
+function descisionMaker() {
+   if (compResp === responseResult) {
+      window.alert("It was a tie! You can play again.");
+      playerScores[2]++;
+   } else
+      if (
+         (compResp === "r" && responseResult === "p") ||
+         (compResp === "s" && responseResult === "p") ||
+         (compResp === "p" && responseResult === "r")
+      ) {
+         window.alert("Sorry, the computer won. What a bummer! You can try again!");
+         playerScores[1]++;
+      } else {
+         {
+            window.alert("You win! Computer is a loser!");
+            playerScores[0]++;
+         }
+      }
+}
+
+
+
+descisionMaker();
