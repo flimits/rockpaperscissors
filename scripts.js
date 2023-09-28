7c8a990e97335e1a1ebe41e38c9895117b2b99ed
Jason Savoie <flimits@gmail.com>
2023-09-28 08:53:56 -0700

    finished he descision and put in a do while loop to just run

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 // Let's play a gave of Rock, Paper and Scissors
 
-var playerScores = [, ,]; // wins and losses and ties
+var playerScores = [0,0,0]; // wins and losses and ties
 var responseResult = ""; // Store the users response
 var compResp = ""; // Store the computers response
 var validValues = ["r", "p", "s"];
@@ -14,26 +14,21 @@ function getUserResponse(userResponse) {
    var userResp = prompt("Enter one of either R, S or P to play ...");
    responseResult = userResp.toLowerCase();
    if (responseResult === "r" || responseResult === "s" || responseResult == "p") {
-      console.log(responseResult + " is a good entry is ok");
    } else {
       alert("You did not enter an R, S, or P. Please try again");
       getUserResponse();
    }
 }
 
-getUserResponse();
-
 function computerResponse() {
    var indexNumber = Math.floor(Math.random() * validValues.length);
    compResp = validValues[indexNumber];
-   console.log(compResp + " is what the computer says");
 }
 
-computerResponse();
 
 function descisionMaker() {
    if (compResp === responseResult) {
-      window.alert("It was a tie! You can play again.");
+      window.alert("Computer says " + compResp + ". And you say " + responseResult + "." + "\nIt was a tie! You can play again.");
       playerScores[2]++;
    } else
       if (
@@ -41,16 +36,23 @@ function descisionMaker() {
          (compResp === "s" && responseResult === "p") ||
          (compResp === "p" && responseResult === "r")
       ) {
-         window.alert("Sorry, the computer won. What a bummer! You can try again!");
+         window.alert("Computer says " + compResp + ". And you say " + responseResult + "." + "\nSorry, the computer won. What a bummer! You can try again!");
          playerScores[1]++;
       } else {
          {
-            window.alert("You win! Computer is a loser!");
+            window.alert("Computer says " + compResp + ". And you say " + responseResult + "." + "\nYou win! Computer is a loser!");
             playerScores[0]++;
          }
       }
 }
 
+function showStats() {
+   window.alert("Here are your stats:" + "\nWins = " + playerScores[0] + "\nLosses = " + playerScores[1] + "\nTies = " + playerScores[2]);
+}
 
-
+do{
+getUserResponse();
+computerResponse();
 descisionMaker();
+showStats();
+} while (true);
